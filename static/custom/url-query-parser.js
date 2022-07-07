7225b41b9a155231f29857c7f8378937ec641e45
AlanGuo <AlanGuo>
2022-07-07 16:33:21 +0800

    update

--- static/custom/url-query-parser.js
+++ static/custom/url-query-parser.js
@@ -13,8 +13,8 @@ let queryString = $.param(getQueryParam())
 if (queryString) {
   const gogodasUrl = `https://app.gogodas.com/explorer?${queryString}`
   const denameUrl = `https://app.dename.com?${queryString}`
-  const dasUrl = `https://app.da.systems/explorer?${queryString}`
-  const bestdasUrl = `https://bestdas.com?${queryString}`
+  const dasUrl = `https://app.did.id/explorer?${queryString}`
+  const bestdasUrl = `https://https://did.top?${queryString}`
   $('#denameUrl').attr('href', denameUrl)
   $('#gogodasUrl').attr('href', gogodasUrl)
   $('#dasUrl').attr('href', dasUrl)
